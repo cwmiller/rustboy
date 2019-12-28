6321a5d1bc894a2ed72215e9ece419251580ea53
Chase Miller <chase@miller.sh>
2019-12-27 20:08:28 -0500

    Timer fixes
    Timer interrupt was being raised too often and crashing Dr Mario. This fixed it.

--- src/timer.rs
+++ src/timer.rs
@@ -20,9 +20,9 @@ impl TacFrequency {
     fn as_cycles(&self) -> usize {
         match *self {
             TacFrequency::Khz4 => 1024,
-            TacFrequency::Khz262 => 16,
+            TacFrequency::Khz16 => 256,
             TacFrequency::Khz65 => 64,
-            TacFrequency::Khz16 => 256
+            TacFrequency::Khz262 => 16
         }
     }
 }
@@ -38,7 +38,9 @@ pub struct Timer {
     tma: u8,
     tac_enabled: bool,
     tac_freq: TacFrequency,
-    tac_counter: usize
+
+    div_counter: usize,
+    tima_counter: usize
 }
 
 impl Timer {
@@ -49,7 +51,8 @@ impl Timer {
             tma: 0,
             tac_enabled: false,
             tac_freq: TacFrequency::Khz4,
-            tac_counter: 0
+            div_counter: 0,
+            tima_counter: 0
         }
     }
 
@@ -58,21 +61,30 @@ impl Timer {
         let mut result = TimerResult::default();
 
         // DIV always counts at a constant interval even if the timer is disabled
-        self.div = self.div.wrapping_add(cycles as u16);
+        // DIV increments once every ~256 machine cycles
+        self.div_counter += cycles;
+
+        if self.div_counter >= 256 {
+            self.div = self.div.wrapping_add(1);
+            self.div_counter -= 256;
+        }
 
         // TIMA counts if TAC is enabled. It counts at an interval set by TAC.
         if self.tac_enabled {
-            self.tac_counter = self.tac_counter + cycles;
-            if self.tac_counter >= self.tac_freq.as_cycles() {
-                let previous = self.tima;
-                self.tima = self.tima.wrapping_add(1);
+            self.tima_counter += cycles;
 
+            let tac_cycles = self.tac_freq.as_cycles();
+
+            if self.tima_counter >= tac_cycles {
                 // When TIMA overflows, it is reset to TMA and an interrupt is raised.
-                if previous > self.tima {
+                if self.tima == 0xFF {
                     self.tima = self.tma;
-                    self.tac_counter = 0;
                     result.interrupt = true;
+                } else {
+                    self.tima += 1;
                 }
+
+                self.tima_counter -= tac_cycles;
             }
         }
 
@@ -96,7 +108,7 @@ impl Addressable for Timer {
             // Writing any value to DIV resets it to 0
             ADDR_DIV => self.div = 0,
             ADDR_TIMA => self.tima = val,
-            ADDR_TMA => self.tima = val,
+            ADDR_TMA => self.tma = val,
             ADDR_TAC => {
                 self.tac_enabled = (val & 0b100) == 0b100;
                 self.tac_freq = TacFrequency::from_u8(val & 0b11).unwrap();
